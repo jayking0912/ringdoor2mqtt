2913087d8fa428520e1bff6cbc6a184ec02eda8f
pi's imac <jayking0912@foxmail.com>
2020-12-22 22:08:10 +0800

    忽略每隔3小时的live信号，待观察

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/kataras/iris/v12"
 	"master/mqtt"
+	"time"
 )
+
+var isAliveSign bool = false
 
 func main(){
 
@@ -16,10 +19,22 @@ func main(){
 
 	//主页
 	app.Get("/bell/attr",  func(ctx iris.Context) {
-
+		if(isAliveSign){
+			isAliveSign = false
+			return
+		}
 		//发送到mqtt客户端
 		mqtt.Mqpublic("node-red/ringdoor",1,false,"{state:true}")
 	})
+
+	app.Get("/bell/upgrade",  func(ctx iris.Context) {
+		//忽略每隔3小时的live信号
+		isAliveSign = true
+		time.AfterFunc(time.Second*2, func() {
+			isAliveSign = false
+		})
+
+		})
 
 	//mqtt 初始化
 	go mqtt.MqInit()
